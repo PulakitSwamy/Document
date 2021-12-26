a94dea40b3febc51591a6493cad6e6967fe9dd52
Pulakit Swamy <93078257+PulakitSwamy@users.noreply.github.com>
2021-12-26 12:20:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var num1 = 20;
+var num2 = 40;
+var num3=60;
+var num4=80;
+var num5=100;
+console.log(num1+num2+num3+num4+num5)
+console.log(num1-num2-num3-num4-num5)
+console.log(num1*num2*num3*num4*num5)
+console.log(num1/num2/num3/num4/num5)
